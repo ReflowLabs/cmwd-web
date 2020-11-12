5bf15764381872184359e6e5043c34eba949d786
Chris Hitchcott <hitchcott@gmail.com>
2020-11-12 11:56:39 +0700

    Customise config and font

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'London After Midnight',
-  shortName: 'LAM',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Vaporwavy',
-  twitter: ''
+  name: "Chiang Mai Web Developer Meetup",
+  shortName: "<CMWD/>",
+  description:
+    "CMWD is a Web Technology knowledge-sharing event held in Chiang Mai, Thailand",
+  url: "https://cmwd.io",
+  prefix: "/",
+  author: "Reflow Labs",
+  twitter: "@reflow__",
 }
